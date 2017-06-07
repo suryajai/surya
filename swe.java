bad063ee02c78a897efa5350a194a29266a4ece2
suryajai <arasidpharm@gmail.com>
2017-06-07 14:59:14 +0530

    Add files via upload

---
+++ swe.java
@@ -0,0 +1,24 @@
+package positive;
+import java.util.Scanner;
+public class positive {
+		public static void main(String args[])
+		{
+		int integer;
+		Scanner in=new Scanner(System.in);
+		System.out.println("Enter the number");
+		no=in.nextInt();
+		if(integer==0)
+		{
+		System.out.println("The given no "+integer+" is zero");
+		}
+		if(integer>0)
+		{
+		System.out.println("The given no "+integer+" is positive");
+		}
+		if(integer<0)
+		{
+		System.out.println("The given no "+integer+" is negative");
+		}
+		}
+		}
+
